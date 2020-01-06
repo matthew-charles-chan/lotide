f2dacac1b466477f30c1b2ef0d76bd16f82cca2f
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-06 23:50:24 +0000

    formatting code with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   console.log(actual === expected ?
-    '✅ Assertion Passed: ' + actual + ' === ' +  expected :
-    '❌ Assertion Failed: ' + actual + ' !== ' +  expected
+    `✅ Assertion Passed: ${actual} === ${expected}` :
+    `❌ Assertion Failed: ${actual} !== ${expected}`
   );
 };
 
