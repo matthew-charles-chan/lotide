359158ac1313f1268ed2f3b7430e5f5f60f5bcc4
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-08 23:13:58 +0000

    using 'c style' loop to index letter position and push to object array

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const assertEqualArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    // if element w/in array does not match corresponding element in other string, or array lengths different, return false
+    if (actual[i] !== expected[i] || actual.length !== expected.length) {
+      console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+      return;
+    }
+  }
+  // esle return true
+  console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+};
+
+
+
+
+const letterPosition = function(str) {
+  // create resultObject
+  let resultObject = {};
+  for (let i = 0; i < str.length; i ++) {
+    if (str[i] !== " ") {
+      if (resultObject[str[i]]) {
+        resultObject[str[i]].push(i);
+      } else {
+        resultObject[str[i]] = [i];
+      }
+    }
+  }
+  return resultObject;
+};
+
+const testLetterPos = letterPosition("hello");
+
+assertEqualArrays(testLetterPos.h, [0]);
+assertEqualArrays(testLetterPos.e, [1]);
+assertEqualArrays(testLetterPos.l, [2,3]);
+assertEqualArrays(testLetterPos.o, [4]);
+
