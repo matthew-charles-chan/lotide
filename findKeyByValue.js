1af92b42d2e6b2ca6e37dd1bb82040bbacb44a04
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-08 23:47:56 +0000

    add findByKeyValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ?
+    `✅ Assertion Passed: ${actual} === ${expected}` :
+    `❌ Assertion Failed: ${actual} !== ${expected}`
+  );
+};
+
+const findKeyByValue = function(obj,value) {
+ let objKeys = Object.keys(obj)
+ for (key of objKeys) {
+   if (obj[key] === value) {
+     return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+//test 
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
