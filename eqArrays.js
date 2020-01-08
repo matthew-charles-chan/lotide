ba54cb9933b1f129f3adfa08609e672e42039732
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-08 00:55:33 +0000

    commenting code

--- eqArrays.js
+++ eqArrays.js
@@ -7,8 +7,10 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(ar1, ar2) {
   for (let i = 0; i < ar1.length; i++) {
+    // if element w/in array does not match corresponding element in other string, or array lengths different, return false
     if (ar1[i] !== ar2[i] || ar1.length !== ar2.length) return false;
   }
+  // esle return true
   return true;
 };
 
