fc07ca9b64ecf7e0f1f64f63d149376382f4e0f0
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-11 02:12:16 +0000

    seperated length and item equality test

--- eqArrays.js
+++ eqArrays.js
@@ -8,7 +8,11 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(actual, expected) {
   for (let i = 0; i < actual.length; i++) {
     // if element w/in array does not match corresponding element in other string, or array lengths different, return false
-    if (actual[i] !== expected[i] || actual.length !== expected.length) return false;
+    if (actual.length !== expected.length) {
+      return false 
+    } else if (actual[i] !== expected[i]) {
+      return false;
+    } 
   }
   // else return true
   return true;
