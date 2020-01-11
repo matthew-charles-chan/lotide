fdbd99499236522ad2256b33a8181560875bd354
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-11 02:39:25 +0000

    fix bug, not able to compare keys

--- eqObjects.js
+++ eqObjects.js
@@ -5,42 +5,41 @@ const assertEqual = function(actual, expected) {
   );
 };
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(objectA, objectB) {
-  // set aKeys and bKeys to ObjectA and ObjectB keys
   const aKeys = Object.keys(objectA);
   const bKeys = Object.keys(objectB);
-  
-  //if number of keys is different ==> FALSE
+  // if objects are different lengths, return false
   if (aKeys.length !== bKeys.length) {
     return false;
   }
-
-  //loop through each key in objectA keys
+  // loop through objectA
   for (let key of aKeys) {
-   
-    // tests if key value is an array
-    if (Array.isArray(objectA[key])) {
-      // if an array, loop through the array
-      for (let aKeyArray of objectA[key]) {
-        // if array values are different or length of array is different ==> FALSE
-        if (objectA[aKeyArray] !== objectB[aKeyArray] || objectA[key].length !== objectB[key].length) {
+    // if object keys are different (objectA key does not exist in objectB), return false
+    if (!objectB[key]) {
+      return false;
+    // if object key is array, loop through array
+    } else if (Array.isArray(objectA[key])) {
+      for (let i = 0; i < objectA[key].length; i ++) {
+        // if array lengths are different, return false
+        if (objectA[key].length !== objectB[key].length) {
+          return false;
+        // if array items are different, return fale
+        } else if (objectA[key][i] !== objectB[key][i]) {
           return false;
         }
       }
-    // if not an array, compare key values, if different ===> FALSE
+    // if key is not an array, check for equality of value. if values not same, return false.
     } else if (objectA[key] !== objectB[key]) {
       return false;
     }
   }
-
-  // else, ==> TRUE
+  // if all keys and values equal, return true
   return true;
 };
+    
 
-const ab = { a: '1', b: '2'};
-const ba = { b: '2', a: '1'};
+const ab = { a: '1', b: [2, 4]};
+const ba = { b: [2, 4], a: '1'};
 assertEqual(eqObjects(ab, ba), true);
 
 
@@ -53,7 +52,7 @@ assertEqual(eqObjects(cd, dc), true);
 
 const ed = { e: "1", d: ["2", 3], f: 7 };
 const de = { d: ["2", 3], e: "1" };
-assertEqual(eqObjects(cd, dc), false);
+assertEqual(eqObjects(ed, de), false);
 
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
