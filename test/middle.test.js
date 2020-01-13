44ffd9f141f9e7a15a854ecd4a59d6f91755dc54
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-13 23:18:28 +0000

    implement mocha with middle.test.js

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,7 +1,16 @@
-const assertEqualArrays = require('../assertArraysEqual');
-const middle = require('../middle')
+const assert = require('chai').assert;
+const middle = require('../middle');
 
+describe('#middle', () => {
+  it('given [1,2,3], return [2]', () => {
+    assert.deepEqual(middle([1,2,3]), [2])
+  });
+  
+  it('given [1,2], return []', () => {
+    assert.deepEqual(middle([1,2]), [])
+  });
 
-assertEqualArrays(middle([1, 2]),[]);
-assertEqualArrays(middle([1, 2, 3]),[2]);
-assertEqualArrays(middle([1, 2, 3, 4]),[2, 3]);
+  it('given [1,2,3,4], return [2,3]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3])
+  });
+});
