903df1cc50afce577ddc27f3e33655b615e807f9
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-13 22:45:38 +0000

    mocha testing on tail

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,14 +1,14 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail')
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]); // result is an Arraay
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const assert = require('chai').assert
 
 
 
-const words = ["hello", "Emma", "Matthew"];
-tail(words);
-assertEqual(words.length, 3);
+describe('#tail', () => {
+  it('given ["Hello", "Lighthouse", "Labs"], return ["Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  });
+
+  it("given [1,2,3,4], return [2,3,4]", () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4])
+  });
+});
