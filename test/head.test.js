5977915a2866548b1283e340902b32f50e1af1d5
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-13 22:30:43 +0000

    added mocha chai

--- test/head.test.js
+++ test/head.test.js
@@ -1,5 +1,24 @@
-const assertEqual = require('../assertEqual')
+// const assertEqual = require('../assertEqual')
+const assert = require('chai').assert
 const head = require('../head')
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe ("#head", () => {
+  it("given [5,6,7], return 5", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("given [1, 2, 3], return 1", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("given ['Hello', 'Lighthouse', 'Labs'], return 'Hello", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+});
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
